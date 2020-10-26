c095f550b940e7261435e4a79dfcc9ed3a05e7b9
Azure-Fluent <azfluent@microsoft.com>
2020-10-26 13:04:46 +0800

    Release azure-resourcemanager 2.0.0

--- src/main/java/com/azure/resourcemanager/compute/samples/ListVirtualMachineImages.java
+++ src/main/java/com/azure/resourcemanager/compute/samples/ListVirtualMachineImages.java
@@ -1,21 +1,20 @@
-/**
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for
- * license information.
- */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-package com.microsoft.azure.management.compute.samples;
+package com.azure.resourcemanager.compute.samples;
 
-import com.microsoft.azure.management.Azure;
-import com.microsoft.azure.management.compute.VirtualMachineImage;
-import com.microsoft.azure.management.compute.VirtualMachineOffer;
-import com.microsoft.azure.management.compute.VirtualMachinePublisher;
-import com.microsoft.azure.management.compute.VirtualMachineSku;
-import com.microsoft.azure.management.resources.fluentcore.arm.Region;
-import com.microsoft.rest.LogLevel;
-
-import java.io.File;
-import java.util.List;
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.http.rest.PagedIterable;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.resourcemanager.compute.models.VirtualMachineImage;
+import com.azure.resourcemanager.compute.models.VirtualMachineOffer;
+import com.azure.resourcemanager.compute.models.VirtualMachinePublisher;
+import com.azure.resourcemanager.compute.models.VirtualMachineSku;
+import com.azure.core.management.Region;
+import com.azure.core.management.profile.AzureProfile;
 
 /**
  * List all virtual machine image publishers and
@@ -26,10 +25,10 @@ public final class ListVirtualMachineImages {
 
     /**
      * Main function which runs the actual sample.
-     * @param azure instance of the azure client
+     * @param azureResourceManager instance of the azure client
      * @return true if sample runs successfully
      */
-    public static boolean runSample(Azure azure) {
+    public static boolean runSample(AzureResourceManager azureResourceManager) {
         final Region region = Region.US_WEST_CENTRAL;
 
         //=================================================================
@@ -38,7 +37,7 @@ public final class ListVirtualMachineImages {
         // published by Canonical, Red Hat and SUSE
         // by browsing through locations, publishers, offers, SKUs and images
 
-        List<VirtualMachinePublisher> publishers = azure
+        PagedIterable<VirtualMachinePublisher> publishers = azureResourceManager
                 .virtualMachineImages()
                 .publishers()
                 .listByRegion(region);
@@ -94,14 +93,18 @@ public final class ListVirtualMachineImages {
             //=================================================================
             // Authenticate
 
-            final File credFile = new File(System.getenv("AZURE_AUTH_LOCATION"));
+            final AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
+            final TokenCredential credential = new DefaultAzureCredentialBuilder()
+                .authorityHost(profile.getEnvironment().getActiveDirectoryEndpoint())
+                .build();
 
-            Azure azure = Azure.configure()
-                    .withLogLevel(LogLevel.NONE)
-                    .authenticate(credFile)
-                    .withDefaultSubscription();
+            AzureResourceManager azureResourceManager = AzureResourceManager
+                .configure()
+                .withLogLevel(HttpLogDetailLevel.BASIC)
+                .authenticate(credential, profile)
+                .withDefaultSubscription();
 
-            runSample(azure);
+            runSample(azureResourceManager);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
